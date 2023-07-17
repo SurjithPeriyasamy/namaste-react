2dc0785d8c3fe347c9bc8d42a0d8e225e9ed43a0
Surjith <mahindhiran005@gmail.com>
2023-07-18 03:48:45 +0530

    Namaste React ep-02

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 const heading = React.createElement("div",{id:"parent"},[
     React.createElement("div",{id:"child1"},[
-        React.createElement("h1",{id:"heading"},"This is from React h1!"),
-        React.createElement("h2",{id:"heading2"},"This is from React h2!")
+        React.createElement("h1",{id:"heading"},"This is Namaste React"),
+        React.createElement("h2",{id:"heading2"},"By Surjith!")
     ]
     ),
     React.createElement("div",{id:"child2"},[
