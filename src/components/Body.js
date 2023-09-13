f78425dc8df37f6a9b139d78c0ac939fd672c4b9
Surjith <mahindhiran005@gmail.com>
2023-09-13 15:55:14 +0530

    completion

--- src/components/Body.js
+++ src/components/Body.js
@@ -29,7 +29,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.3410364&lng=77.7171642"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.3410364&lng=77.7171642&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
 
     const json = await data.json();
